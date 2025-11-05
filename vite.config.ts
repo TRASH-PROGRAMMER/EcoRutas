7ad1937013b33721f03017300b20d95dc6fde293
KellyDay612 <kellycanchingre@hotmail.com>
2025-11-04 23:31:42 -0500

    actualización general del proyecto y módulo AddLocalidad

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+
+    // 👇 Esta parte es la que evita el error "UNKNOWN: watch"
+    watch: {
+      usePolling: true,  // fuerza el modo polling en lugar de fs.watch
+      interval: 1000     // revisa cambios cada 1 segundo
+    },
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
